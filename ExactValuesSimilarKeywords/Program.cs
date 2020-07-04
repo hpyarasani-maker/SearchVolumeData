9e9f667e367ca4505c8bebf33a253a9c1504fd0a
UKUser <UKUser@HEMACHANDER>
2020-07-04 11:47:22 +0100

    udpated

--- ExactValuesSimilarKeywords/Program.cs
+++ ExactValuesSimilarKeywords/Program.cs
@@ -1182,7 +1182,7 @@ namespace ExactValuesSimilarKeywords
                     r++;
                 }
             }
-            catch (Exception ex)
+            catch (Exception)
             {
             }
 
