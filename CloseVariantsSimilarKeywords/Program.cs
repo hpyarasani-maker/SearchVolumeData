dd337f31361dd66cf706513c7e9db86cbba96432
UKUser <UKUser@HEMACHANDER>
2020-07-27 12:19:44 +0100

    Revert "updated kwd empty value issue in closevariantsimilarkeywords."
    This reverts commit f7cd119bd839b2379b4586fb55a6f032cbd28028.

--- CloseVariantsSimilarKeywords/Program.cs
+++ CloseVariantsSimilarKeywords/Program.cs
@@ -292,9 +292,6 @@ namespace CloseVariantsSimilarKeywords
 
                             //}
                             //02 - 07 - 2020
-                            
-                            if (string.IsNullOrEmpty(vWord.Trim()))
-                                vWord = "NoData";
 
                             ProcessResultsKPOLD_48(market, kws,country);
                             // DeleteFile(exactpath);
