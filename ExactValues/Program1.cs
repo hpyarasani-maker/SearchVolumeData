c5fdb8051bd4afa497a2d86a3254b9a88ffdac40
UKUser <UKUser@HEMACHANDER>
2020-11-25 15:30:54 +0000

    updated exception at line number 369

--- ExactValues/Program1.cs
+++ ExactValues/Program1.cs
@@ -363,10 +363,10 @@ namespace ExactValues
                                     Console.WriteLine("=====Country Selected Successfully=====");
 
                             }
-                            catch
+                            catch(Exception ex)
                             {
                                 //If any error occured in country selection then signout and exit..
-                                Console.WriteLine("==Problem In Country Selection==");
+                                Console.WriteLine("==Problem In Country Selection=={0]",ex.Message.ToString());
                                 Signout(driver);
                                 driver.Close();
                                 driver.Dispose();
