cbc3cd473ed2f5918c00c739b1b4bc3ab85005a1
UKUser <UKUser@HEMACHANDER>
2020-12-03 17:17:33 +0000

    updated

--- ExactValuesSimilarBatchKeywrods/Program.cs
+++ ExactValuesSimilarBatchKeywrods/Program.cs
@@ -1287,8 +1287,8 @@ namespace ExactValuesSimilarBatchKeywords
                             //string kwd = WebUtility.HtmlDecode(s);
                             string[] values1 = GetMonthValues(s, monthsList);
 
-                            //01-12-2020
-                            if (!string.IsNullOrEmpty(values1[3]) && string.IsNullOrEmpty(values1[12]))
+                            //03-12-2020
+                            if (!string.IsNullOrEmpty(values1?[3]) && string.IsNullOrEmpty(values1?[12]))
                             {
                                 Console.WriteLine("========================================================");
                                 Console.WriteLine("------------------ Ranges Started ----------------------");
