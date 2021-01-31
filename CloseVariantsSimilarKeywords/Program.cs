55479994955a08850b8a4e495012ee04b4186cfd
UKUser <UKUser@HEMACHANDER>
2021-01-31 16:22:40 +0000

    update

--- CloseVariantsSimilarKeywords/Program.cs
+++ CloseVariantsSimilarKeywords/Program.cs
@@ -114,11 +114,7 @@ namespace CloseVariantsSimilarKeywords
                 Console.WriteLine(driver.PageSource);
                 driver.FindElement(By.CssSelector("material-list-item.user-customer-list-item:nth-child(1)")).Click();//28-01-2021 updated selector for cancelled account click event
             }
-            catch(Exception ex) {
-                Console.WriteLine(driver.PageSource);
-                driver.FindElement(By.CssSelector("material-list-item.user-customer-list-item:nth-child(1)")).Click();//28-01-2021 updated selector for cancelled account click event
-
-            }
+            catch { }
             ////////////////////
             
             while (true)
