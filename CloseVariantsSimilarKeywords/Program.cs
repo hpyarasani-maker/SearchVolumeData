8595ab0226092062dd4ac066c5e0c26087c65976
UKUser <UKUser@HEMACHANDER>
2021-02-01 15:38:42 +0000

    change time

--- CloseVariantsSimilarKeywords/Program.cs
+++ CloseVariantsSimilarKeywords/Program.cs
@@ -35,7 +35,7 @@ namespace CloseVariantsSimilarKeywords
             string exactpath = @"C:\inetpub\wwwroot\closevariants\" + email.Split('@')[0];  //closevariants folder must in wwwroot
             DeleteFile(exactpath); //28-01-2021
             Console.Title = title + " - With Timer 26 minutes";
-            Timer t = new Timer(DisplayTimeEvent, null, (28 * 60000), 1000);
+            Timer t = new Timer(DisplayTimeEvent, null, (26 * 60000), 1000);
             Console.Title = title;
             System.Drawing.Size size = new System.Drawing.Size(1280, 1024);
             // ChromeOptions chromeOptions = new ChromeOptions();
